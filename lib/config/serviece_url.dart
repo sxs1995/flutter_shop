08785ef00f3fbbd159c078b61e77d16ae420ea7b
shenxsh <shenxsh@inspurworld.com>
2019-03-26 17:08:45 +0800

    分类页面左侧列表

--- lib/config/serviece_url.dart
+++ lib/config/serviece_url.dart
@@ -3,4 +3,5 @@ const serviceUrl = 'http://test.baixingliangfan.cn/baixing/';
 const servicePath = {
   "homePageContent": serviceUrl + 'wxmini/homePageContent', //商店首页信息
   "homepagebelowConten": serviceUrl + 'wxmini/homePageBelowConten', //商城首页火爆商品
+  "getCategory": serviceUrl + 'wxmini/getCategory', //商城首页火爆商品
 };
--- lib/pages/category.dart
+++ lib/pages/category.dart
@@ -1,16 +1,96 @@
 import 'package:flutter/material.dart';
+import '../config/serviece_url.dart';
+import '../service/service_method.dart';
+import 'dart:io';
+import 'dart:convert';
+import '../model/category.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class CategoryPage extends StatelessWidget {
+class CategoryPage extends StatefulWidget {
   final Widget child;
 
   CategoryPage({Key key, this.child}) : super(key: key);
 
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('商品分类'),
+      ),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            LeftCategoryNav(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//左侧大类导航
+class LeftCategoryNav extends StatefulWidget {
+  final Widget child;
+
+  LeftCategoryNav({Key key, this.child}) : super(key: key);
+
+  _LeftCategoryNavState createState() => _LeftCategoryNavState();
+}
+
+class _LeftCategoryNavState extends State<LeftCategoryNav> {
+  List list = [];
+
+  @override
+  void initState() {
+    _getCategory();
+    super.initState();
+  }
+
+  void _getCategory() async {
+    await request('getCategory').then((val) {
+      var data = json.decode(val.toString());
+      CategoryModel category = CategoryModel.fromJson(data);
+      setState(() {
+        list = category.data;
+      });
+      // List.data.forEach((item) => print(item.mallCategoryName));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('分类'),
+      width: ScreenUtil().setWidth(180),
+      decoration: BoxDecoration(
+          border: Border(right: BorderSide(width: 0.5, color: Colors.black12))),
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _leftInkwell(index);
+        },
       ),
+    );
+  }
+
+  Widget _leftInkwell(int index) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+          height: ScreenUtil().setHeight(100),
+          padding: EdgeInsets.only(left: 10.0, top: 16.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border:
+                Border(bottom: BorderSide(width: 0.5, color: Colors.black12)),
+          ),
+          child: Text(
+            list[index].mallCategoryName,
+            style: TextStyle(fontSize: ScreenUtil().setSp(28)),
+          )),
     );
   }
 }
